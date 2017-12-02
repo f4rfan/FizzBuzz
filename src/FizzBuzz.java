9b9216230fbbf25c82e36b68ed342773bf8f87d5
farfan <sergio_farfan_mp@hotmail.com>
2017-12-01 19:47:18 -0600

    Ejercicio del 30 de noviembre

---
+++ src/FizzBuzz.java
@@ -0,0 +1,26 @@
+
+public class FizzBuzz {	public static String x = "";
+	
+	public static String Resultado(int i) {
+		boolean fb = false;
+		if (i % 3 == 0) {
+			x = x+"Fizz";
+			fb = true;
+    	}
+        	
+        if (i % 5 == 0) {
+        	x = x+"Buzz";
+        	fb = true;
+        }
+        
+        if (fb == true) {
+        	return x;
+        }
+        
+        else {
+        	return String.valueOf(i);
+        }
+             
+	}
+	
+}
---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+	@Test
+	void test() {
+		assertEquals("Fizz", FizzBuzz.Resultado(3), "Debe regresar 1");
+	}
+
+}
